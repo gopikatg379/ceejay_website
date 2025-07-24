535239d399516608b84de9a24265589bf68d7ed2
Gopika T G <gopikatg379@gmail.com>
2025-07-24 12:55:48 +0530

    Initialised

---
+++ Adminapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def about_us(request):
+    return render(request, 'about.html')
+
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def careers(request):
+    return render(request, 'careers.html')
+
+
+def why_us(request):
+    return render(request, 'why_us.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
